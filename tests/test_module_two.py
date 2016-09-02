fb24d398f0f2993ca1bf3566c05ab76d71164624
prabhath6 <prabhathkiran20@gmail.com>
2016-09-02 11:49:54 -0700

    Modify to nosetests

--- tests/test_module_two.py
+++ tests/test_module_two.py
@@ -1,10 +1,24 @@
-import unittest
 from my_project import module_two
+from nose.tools import assert_equal
 
 
-class BasicTestSuitePalindrome(unittest.TestCase):
+class TestModuleTwo():
+
+    @classmethod
+    def setup_class(klass):
+        """This method is run once for each class before any tests are run"""
+
+    @classmethod
+    def teardown_class(klass):
+        """This method is run once for each class _after_ all tests are run"""
+
+    def setUp(self):
+        """This method is run once before _each_ test method is executed"""
+
+    def teardown(self):
+        """This method is run once after _each_ test method is executed"""
 
     def test_module_two(self):
         expected = module_two.custom_count("AAAAsdfsdvver", 3)
         actual = [('A', 4)]
-        self.assertEqual(expected, actual)
+        assert_equal(expected, actual)
